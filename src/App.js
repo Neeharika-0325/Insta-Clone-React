e8264c1ef7d613986c7cbae81b5b8a19143f87aa
Neeharika-0325 <neeharikamarella3@gmail.com>
2023-03-31 11:12:54 +0530

    Added node api web service into env variable

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LandingPage from './components/Landing_page';
 import PostView from './components/Postview';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Upload from "./components/Upload"
-const API_KEY ="http://localhost:8080"
+const API_KEY =process.env.REACT_API || "http://localhost:8080"
 
 const App = ()=>{
   const [userData , setuserData] = useState([])
